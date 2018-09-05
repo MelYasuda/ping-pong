ad4f1ba2004a84964729805044eead514aa8f290
Hyewon Cho and Meguru Yasuda <hyewon@>
2018-09-05 11:43:07 -0700

    pass spec number 3, 4 and 5

--- ping-pong.Tests/ModelTests/PingpongTest.cs
+++ ping-pong.Tests/ModelTests/PingpongTest.cs
@@ -1,17 +1,38 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Pingpong;
+using System.Collections.Generic;
 
 namespace Pingpong.Tests
 {
   [TestClass]
   public class PingpongTest
   {
+    List <int> emptyList = new List<int>();
     [TestMethod]
-    public void ShowOutput_GetUserInput_True()
+    public void ShowOutput_GetUserInput()
     {
       PingpongGenerator generator = new PingpongGenerator();
-      Assert.AreEqual(false, generator.ShowOutput(""));
-      Assert.AreEqual(true, generator.ShowOutput("32"));
+      CollectionAssert.AreEqual(emptyList, generator.ShowOutput(""));
+    }
+    [TestMethod]
+    public void ShowOutput_ValidateUserInput()
+    {
+      PingpongGenerator generator = new PingpongGenerator();
+      CollectionAssert.AreEqual(emptyList, generator.ShowOutput("a"));
+      CollectionAssert.AreEqual(emptyList, generator.ShowOutput("-32"));
+      CollectionAssert.AreEqual(emptyList, generator.ShowOutput("0"));
+    }
+    [TestMethod]
+    public void ShowOutput_OneToInputNumber()
+    {
+      PingpongGenerator generator = new PingpongGenerator();
+      CollectionAssert.AreEqual(new List<int>{1,2}, generator.ShowOutput("2"));
+    }
+    [TestMethod]
+    public void ShowOutput_ReplaceMultiplesOf3WithPing()
+    {
+      PingpongGenerator generator = new PingpongGenerator();
+      CollectionAssert.AreEqual(new List<int>{1,2,-1,4,5,-1}, generator.ShowOutput("6"));
     }
   }
 }
--- ping-pong/Models/Pingpong.cs
+++ ping-pong/Models/Pingpong.cs
@@ -5,17 +5,42 @@ namespace Pingpong
 {
   public class PingpongGenerator
   {
-    public bool ShowOutput(string input)
+    public List<int> ShowOutput(string input)
     {
+      List <int> list = new List<int>();
       // if input is an empty string return false
-        if (input == "")
+      if (input == "")
+      {
+        return list;
+      }
+      else
+      {
+        int parsedNumber = 0;
+        bool parseResult = int.TryParse(input, out parsedNumber);
+        if (parseResult == false)
         {
-          return false;
+          return list;
+        }
+        else if (parsedNumber < 1)
+        {
+          return list;
         }
         else
         {
-          return true;
+          for (int i = 1; i <= parsedNumber; i++)
+          {
+            if (i % 3 == 0)
+            {
+              list.Add(-1);
+            }
+            else
+            {
+              list.Add(i);
+            }
+          }
+          return list;
         }
+      }
     }
   }
 }
